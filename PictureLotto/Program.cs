f9485ed147d26967fe42654041eb7d7fc39865c7
GET Academy <terje@getacademy.no>
2022-05-05 09:49:08 +0200

    Update Program.cs

--- PictureLotto/Program.cs
+++ PictureLotto/Program.cs
@@ -1,5 +1,12 @@
 ﻿using PictureLotto;
 
+const string baseSymbols = "ABCDEFGHIJ";
+var symbols = new string((baseSymbols + baseSymbols)
+    .ToCharArray()
+    .OrderBy(x => Guid.NewGuid())
+    .ToArray()
+);
+
 const int boxesPerRow = 4;
 const int gapCol = 10;
 const int gapRow = 4;
@@ -9,14 +16,8 @@ for (var i = 0; i < 20; i++)
     var colIndex = i % boxesPerRow;
     var consoleRow = rowIndex * gapRow + 1;
     var consoleCol = colIndex * gapCol + 2;
-    if (i % 2 == 0)
-    {
-        GameConsole.WriteClosedBox(consoleRow, consoleCol, i);
-    }
-    else
-    {
-        var c = (char)('A' + i);
-        GameConsole.WriteOpenBox(consoleRow, consoleCol,i, c);
-    }
+    var c = symbols[i];
+    if (i % 2 == 0) GameConsole.WriteClosedBox(consoleRow, consoleCol, i);
+    else GameConsole.WriteOpenBox(consoleRow, consoleCol, i, c);
 }
 
